d2f49843fdbd5e50e34e7f7c6480843f3596d251
rithika02tech <rithikavyas02@gmail.com>
2024-11-28 22:33:05 +0530

    Add files via upload

---
+++ area(28-11-24).py
@@ -0,0 +1,41 @@
+#Area of rectangle
+
+length=int(input("Enter the length : "))
+
+breadth=int(input("Enter the breadth : "))
+
+area=length*breadth
+
+print("Area of rectangle : ",area)
+
+
+
+#Area of Square
+
+side=int(input("Enter the side : "))
+
+area=side*side
+
+print("Area of Square : ",area)
+
+
+
+#Area of circle
+
+radius=int(input("Enter the radius : "))
+
+area=3.14*(radius*radius)
+
+print("Area of circle : ",area)
+
+
+
+#Area of triangle
+
+base=int(input("Enter the base : "))
+
+height=int(input("Enter the height : "))
+
+area=1/2*base*height
+
+print("Area of triangle : ",area)
